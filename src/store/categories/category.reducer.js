1f1ce3dbb752df5850fff3bc4e43ecb76d961842
Ngoc.Nguyen <ntlngoc239@gmail.com>
2023-05-24 23:21:33 +0700

    initial redux toolkit

---
+++ src/store/categories/category.reducer.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import { CATEGORIES_ACTION_TYPES } from './category.types';
+
+export const CATEGORIES_INITIAL_STATE = {
+  categories: [],
+};
+
+export const categoriesSlice = createSlice({
+  name: 'category',
+  initialState: CATEGORIES_INITIAL_STATE,
+  reducers: {
+    selectCategoriesMap(state, action) {
+      state.categories = action.payload.categories;
+    }
+  }
+});
+export const { selectCategoriesMap } = categoriesSlice.actions;
+
+export const categoriesReducer = categoriesSlice.reducer;
+// (
+//   state = CATEGORIES_INITIAL_STATE,
+//   action = {}
+// ) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+//       return { ...state, categories: payload };
+//     default:
+//       return state;
+//   }
+// };
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,21 @@
+// import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from "@reduxjs/toolkit"
+// createSlice generating three files (action, reducer, type )
+const INITIAL_STATE = {
+  currentUser: null
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: INITIAL_STATE,
+  reducers: {
+    //setCurrentUser: () => {}
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    }
+  }
+})
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
